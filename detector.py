1781575271c323b1385ba06209d4afac607aaae7
EzzyWizzy <39401973+EzzyWizzy@users.noreply.github.com>
2022-07-22 01:30:40 +0300

    Object Detection

--- detector.py
+++ detector.py
@@ -40,7 +40,7 @@ while True:
                 traffic_state4(0, 1, 0)
                 traffic_state2(1, 0, 0)
                 traffic_state3(1, 0, 0) 
-            elif classNames[classId-1] == 'laptop'::
+            elif classNames[classId-1] == 'laptop':
                 off_all(0, 0, 0)
                 traffic_state(1, 0, 0)
                 traffic_state4(1, 0, 0)
