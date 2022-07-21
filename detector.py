9df1d2bf3e7574b16b2ba0fdac6eac592d3caa93
EzzyWizzy <39401973+EzzyWizzy@users.noreply.github.com>
2022-07-22 00:20:50 +0300

    Object Detection

--- detector.py
+++ detector.py
@@ -1,6 +1,11 @@
 import cv2
 
-img = cv2.imread('person.jpg')
+#img = cv2.imread('person.jpg')
+
+cap = cv2.VideoCapture(1)
+cap.set(3,640)
+cap.set(4,480)
+
 classNames=[]
 classFile='model_data/coco.names'
 with open(classFile, 'rt') as f:
@@ -17,9 +22,15 @@ net.setInputScale(1.0/ 127.5)
 net.setInputMean((127.5,127.5,127.5))
 net.setInputSwapRB(True)
 
-classIds, confs, bbox = net.detect(img,confThreshold=0.5)
-print(classIds,bbox)
-
-cv2.imshow("output",img)
-cv2.waitKey(0)
+while True:
+    success, img =cap.read()
+    classIds, confs, bbox = net.detect(img,confThreshold=0.5)
+    #print(classIds,bbox)
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            cv2.rectangle(img, box, color=(0,255,0), thickness=2)
+            cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30), cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2) 
+            print(classNames[classId-1])
+    cv2.imshow("output",img)
+    cv2.waitKey(1)
 
